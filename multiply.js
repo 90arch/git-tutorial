48e3f1f1890c73c40c3f14040df4826fb5e18c2e
archana <archana.gupta458@gmail.com>
2022-11-04 17:27:07 +0530

    adding multiply.js

---
+++ multiply.js
@@ -0,0 +1,6 @@
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter second number"));
+
+var multiply = number1 + number2;
+
+console.log(`${number1} * ${number2} = ${multiply}`);
